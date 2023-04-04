e2e3bda12a3e8590512b2951581904bed14024da
elisaveta9 <lizashishova9@gmail.com>
2023-04-05 00:29:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/voiceassistent/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.voiceassistent
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    lateinit var sendButton : Button
+    lateinit var chatWindow: TextView
+    lateinit var questionText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sendButton = findViewById(R.id.sendButton)
+        chatWindow = findViewById(R.id.chatWindow)
+        questionText = findViewById(R.id.questionField)
+
+        sendButton.setOnClickListener {
+            onSend()
+        }
+    }
+
+    private fun onSend(){
+        val text = questionText.text.toString()
+        val answer = "Вопрос понял. Думаю…"
+        dismissKeyboard()
+    }
+
+    private fun dismissKeyboard() {
+        val view: View? = this.currentFocus // элемент, который имеет текущий фокус ввода
+        if (view != null) {
+            // определить менеджер, отвечающий  за ввод
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            // менеджер скрывает экранную клавиатуру
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+}
