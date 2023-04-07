7e7dfa717a957f3d5a4e3a3a527b01bd76904901
elisaveta9 <lizashishova9@gmail.com>
2023-04-07 21:14:52 +0300

    Update files

--- app/src/main/java/com/example/voiceassistent/AI.kt
+++ app/src/main/java/com/example/voiceassistent/AI.kt
@@ -1,18 +1,42 @@
 package com.example.voiceassistent
 
-class AI {
-    val queAndAns = mapOf(
-        Regex(".*((привет(ик)?)|(здравствуй)).*") to "Привет. ",
-        Regex(".*как (дел(а|(ишки)))\\?.*") to "Не плохо. ",
-        Regex(".*((а )?чем занимаешься\\?).*") to "Отвечаю на вопросы. "
+import android.content.Context
+import java.time.DayOfWeek
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class AI(context: Context) {
+    private val daysOfWeek = mapOf(
+        DayOfWeek.MONDAY to context.getString(R.string.day_of_week_monday),
+        DayOfWeek.TUESDAY to context.getString(R.string.day_of_week_tuesday),
+        DayOfWeek.WEDNESDAY to context.getString(R.string.day_of_week_wednesday),
+        DayOfWeek.THURSDAY to context.getString(R.string.day_of_week_thursday),
+        DayOfWeek.FRIDAY to context.getString(R.string.day_of_week_friday),
+        DayOfWeek.SATURDAY to context.getString(R.string.day_of_week_saturday),
+        DayOfWeek.SUNDAY to context.getString(R.string.day_of_week_sunday)
+    )
+    private val queAndAns = mapOf(
+        context.getString(R.string.hello_pattern).toRegex() to
+                context.getString(R.string.hello_answer),
+        context.getString(R.string.how_are_you_pattern).toRegex() to
+                context.getString(R.string.how_are_you_answer),
+        context.getString(R.string.what_are_you_doing_pattern).toRegex() to
+                context.getString(R.string.what_are_you_doing_answer),
+        context.getString(R.string.date_today_pattern).toRegex() to LocalDateTime.now()
+            .format(DateTimeFormatter.ofPattern("dd MMM yyyy")).toString(),
+        context.getString(R.string.what_time_is_it_pattern).toRegex() to LocalDateTime.now()
+            .format(DateTimeFormatter.ofPattern("hh:mm:ss a")).toString(),
+        context.getString(R.string.day_of_the_week_today_pattern).toRegex() to
+                daysOfWeek.get(LocalDate.now().dayOfWeek)
     )
 
-    public fun getAnswer(question:String):String{
+    fun getAnswer(question:String):String{
         question.lowercase()
         var answer = ""
         for ((key, value) in queAndAns.entries){
             if (question.matches(key))
-                answer += value
+                answer += "$value "
         }
         if (answer == "")
             answer = "Вопрос понял. Думаю..."
--- app/src/main/java/com/example/voiceassistent/MainActivity.kt
+++ app/src/main/java/com/example/voiceassistent/MainActivity.kt
@@ -2,25 +2,32 @@ package com.example.voiceassistent
 
 import android.content.Context
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var sendButton : Button
     lateinit var chatWindow: TextView
     lateinit var questionText: EditText
+    lateinit var textToSpeech: TextToSpeech
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Locale.setDefault(Locale("ru"))
 
         sendButton = findViewById(R.id.sendButton)
         chatWindow = findViewById(R.id.chatWindow)
         questionText = findViewById(R.id.questionField)
+        textToSpeech = TextToSpeech(applicationContext, TextToSpeech.OnInitListener {
+            if (it != TextToSpeech.ERROR) {    textToSpeech.language = Locale.getDefault()}
+        })
 
         sendButton.setOnClickListener {
             onSend()
@@ -30,8 +37,9 @@ class MainActivity : AppCompatActivity() {
     private fun onSend() {
         val text = questionText.text.toString()
         chatWindow.append(text + "\n")
-        val answer = AI().getAnswer(text)
+        val answer = AI(context = applicationContext).getAnswer(text)
         chatWindow.append(answer + "\n")
+        textToSpeech.speak(answer, TextToSpeech.QUEUE_FLUSH,null, null )
         questionText.text.clear()
         dismissKeyboard()
     }
