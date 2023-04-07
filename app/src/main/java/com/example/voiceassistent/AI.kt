a21b0e160740d224610927f1f7c693cdce44671f
elisaveta9 <lizashishova9@gmail.com>
2023-04-07 22:21:22 +0300

    Update files

--- app/src/main/java/com/example/voiceassistent/AI.kt
+++ app/src/main/java/com/example/voiceassistent/AI.kt
@@ -3,7 +3,7 @@ package com.example.voiceassistent
 import android.content.Context
 import java.time.DayOfWeek
 import java.time.LocalDate
-import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
 
 class AI(context: Context) {
@@ -19,16 +19,27 @@ class AI(context: Context) {
     private val queAndAns = mapOf(
         context.getString(R.string.hello_pattern).toRegex() to
                 context.getString(R.string.hello_answer),
+
         context.getString(R.string.how_are_you_pattern).toRegex() to
                 context.getString(R.string.how_are_you_answer),
+
         context.getString(R.string.what_are_you_doing_pattern).toRegex() to
                 context.getString(R.string.what_are_you_doing_answer),
-        context.getString(R.string.date_today_pattern).toRegex() to LocalDateTime.now()
-            .format(DateTimeFormatter.ofPattern("dd MMM yyyy")).toString(),
-        context.getString(R.string.what_time_is_it_pattern).toRegex() to LocalDateTime.now()
-            .format(DateTimeFormatter.ofPattern("hh:mm:ss a")).toString(),
+
+        context.getString(R.string.date_today_pattern).toRegex() to
+                context.getString(R.string.date_today_answer).replace("{CurrentDate}",
+                LocalDate.now().format(DateTimeFormatter
+                    .ofPattern("dd MMM yyyy")).toString()),
+
+        context.getString(R.string.what_time_is_it_pattern).toRegex() to
+                context.getString(R.string.what_time_is_it_answer).replace(
+                    "{CurrentTime}",
+                    LocalTime.now().format(DateTimeFormatter.ofPattern("kk:mm:ss")).
+                    toString()),
+
         context.getString(R.string.day_of_the_week_today_pattern).toRegex() to
-                daysOfWeek.get(LocalDate.now().dayOfWeek)
+                context.getString(R.string.day_of_the_week_today_answer).
+                replace("{CurrentDayOfWeek}", daysOfWeek.get(LocalDate.now().dayOfWeek)!!)
     )
 
     fun getAnswer(question:String):String{
