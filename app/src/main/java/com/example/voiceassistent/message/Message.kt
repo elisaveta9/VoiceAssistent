dc7ecd55a8c794411448423e786a16e422b67b88
Elisaveta <89607033+elisaveta9@users.noreply.github.com>
2023-05-19 19:34:19 +0300

    Delete app/src/main/java/com/example/voiceassistent/message directory

--- app/src/main/java/com/example/voiceassistent/message/Message.kt
+++
@@ -1,7 +0,0 @@
-package com.example.voiceassistent.message
-
-import java.util.*
-
-data class Message(val text: String, val date: Date, val isSend: Boolean){
-    constructor(text: String, isSend: Boolean) : this(text, Date(), isSend)
-}
